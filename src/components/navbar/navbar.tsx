571a39dd8c728bf3908dd014e8cfc91e092ae235
Wanda Syahputra <wanda.syahputra@jd.id>
2022-09-11 21:47:26 +0700

    add pagination

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,18 +1,56 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { CollectionSVG } from '../../assets/img/collection'
 import { IconListSVG } from '../../assets/img/list'
+import { css } from '@emotion/css'
+import { useLocation } from 'react-router-dom';
 
 function Navbar() {
+  const location = useLocation();
+  const navbarStyle = css`
+    display: flex;
+    background-color: rgb(39, 39, 39);
+    position: fixed;
+    bottom: 0px;
+    width: 100%;
+    border-top: solid 1px rgb(70, 70, 70);
+    a {
+      color: white;
+      display: flex;
+      flex-grow: 1;
+      justify-content: center;
+      align-items: center;
+      text-decoration: none;
+      font-weight: 400;
+      transition: .3s;
+      padding: 10px;
+      svg {
+        padding: 0 5px;
+      }
+      &.active {
+        color: #009442;
+        svg {
+          fill: #009442;
+        }
+      }
+    }
+  `
+
   return (
-    <div>
-      <Link to="/">
+    <div className={navbarStyle}>
+      <Link to="/" className={location.pathname === '/' ? 'active' : '' }>
         <IconListSVG
           width={25}
-        />
+          fill="white"
+          />
         AnimeList
       </Link>
-      <Link to="/tag">
-        Tag
+      <Link to="/collection" className={location.pathname === '/collection' ? 'active' : '' }>
+        <CollectionSVG
+          width={25}
+          fill="white"
+        />
+        Collection
       </Link>
     </div>
   )
--- src/routerList.tsx
+++ src/routerList.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/navbar/navbar'
 import AnimeList from './pages/animeList'
+import { css } from '@emotion/css'
 
 function RouterList() {
+  const routerStyle = css`margin-bottom: 45px`
   return (
-    <div>
+    <div className={routerStyle}>
       <Routes>
         <Route path='/' element={<AnimeList />} />
       </Routes>
